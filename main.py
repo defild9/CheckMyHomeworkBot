dd55c94e181fe76a8db1ddbc6182146d2e06544e
Eugune Beletskiy <biletskijzena@protonmail.com>
2022-10-18 21:33:26 +0300

    Made some changes

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ class UserData(StatesGroup):
 class ShowData(StatesGroup):
     show_data_by_subject = State()
 
+class DeleteData(StatesGroup):
+    subject_delete = State()
+    task_delete = State()
+
 Token = "5503220888:AAFafBXfb72XjCxnGLhhr9HDVAId3C3hhgY"
 storage = MemoryStorage()
 bot = Bot(token=Token)
@@ -33,7 +37,7 @@ async def add_homework(message: types.Message):
 async def set_text(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['task'] = message.text
-    await message.reply("Введіть предмет:")
+    await message.reply("Введіть предмет:", reply_markup=kb.sub_menu)
     await UserData.next()
 
 @dp.message_handler(state=UserData.subject)
@@ -41,7 +45,7 @@ async def set_subject(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['subject'] = message.text
         data['id'] = message.from_user.id
-    await message.answer(f"Task:{data['task']}\nSubject:{data['subject']}\nYourId:{data['id']}")
+    await message.answer(f"Task:{data['task']}\nSubject:{data['subject']}\nYourId:{data['id']}",reply_markup=kb.manu)
     db.add_task(str(data['id']), str(data['task']), str(data['subject']))
     await state.finish()
 #Show task
@@ -56,6 +60,11 @@ async def set_show_task_by_subject(message: types.Message, state:FSMContext):
         show['sub_for_show'] = message.text
     await  message.answer(db.show_task(show['sub_for_show']))
     await state.finish()
+
+#delete task
+@dp.message_handler(lambda message: message.text == "❌Видалити завдання")
+async def delete_task(message:types.Message):
+    await message.answer("Введіть назву предмета в якому треба удалити завдання:")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
